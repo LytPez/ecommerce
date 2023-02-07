660588356075e24ab1a86728367ce8c0ce14c208
Guilherme <guilhermeisk@hotmail.com>
2023-02-07 18:11:56 -0300

    fim do dia

--- vendor/hcodebr/php-classes/src/Model/Cart.php
+++ vendor/hcodebr/php-classes/src/Model/Cart.php
@@ -130,6 +130,55 @@ class Cart extends Model{
         return Product::checkList($rows);
   }
  
+  public function getProductsTotals(){
+
+    $sql = new Sql();
+
+    $results = $sql->select(
+      "SELECT SUM(vlprice) AS vlprice, SUM(vlwidth) AS vlwidth, SUM(vlheight) AS vlheight, SUM(vllength) AS vllength, SUM(vlweight) AS vlweight, COUNT(*) AS nrqtd
+      FROM tb_products a
+      INNER JOIN tb_cartsproducts b ON a.idproduct = b.idproduct
+      WHERE b.idcart = :idcart AND dtremoved IS NULL;    
+    ",[
+      ':idcart'=>$this->getidcart()
+    ]);
+
+    if(count($results) > 0){
+      return $results[0];
+    } else{
+      return[];
+    }
+
+  }
+
+  public function setFreight($nrzipcode){
+    $nrzipcode = str_replace('-', '', $nrzipcode);
+
+    $totals = $this->getProductsTotals();
+
+    if($totals['nrqtd'] > 0){
+
+      $qs = http_build_query([
+        'nCdEmpresa'=>'',
+        'sDsSenha'=>'',
+        'nCdServico'=>'40010',
+        'sCepOrigem'=>'09853120',
+        'sCepDestino'=>$nrzipcode,
+        'nrzipcode'=>$totals['vlweight'],
+        'nCdFormato'=>'1',
+        'nVlComprimento'=>$totals['vllength'],
+        'nVlAltura'=>$totals['vlheight'],
+        'nVlLargura'=>$totals['vlwidth'],
+        'nVlDiametro'=>'',
+        'sCdMaoPropria'=>'S',
+        'nVlValorDeclarado'=>'S',
+        'sCdAvisoRecebimento'=>'S'
+      ]);
+
+      $xml = simplexml_load_file("http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx/CalcPrecoPrazo?");
+    }
+  }
+
 }
 
 ?>
